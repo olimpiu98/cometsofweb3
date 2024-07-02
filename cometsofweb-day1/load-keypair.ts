26197608613e9533c79c457b100dbab624cd844e
Olimpiu <ticudean.o@gmail.com>
2024-07-02 17:25:50 +0100

    Day 1 - generate and load keypair

--- cometsofweb-day1/load-keypair.ts
+++ cometsofweb-day1/load-keypair.ts
@@ -5,7 +5,7 @@ import bs58 from "bs58";
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 const privatKey = bs58.encode(keypair.secretKey);
 console.log(
-    `✅ Our public key is: ${keypair.publicKey.toBase58()}`
+    `📢 Our public key is: ${keypair.publicKey.toBase58()}`
 );
 
 console.log(`🔐 Private key: ${privatKey}`);
