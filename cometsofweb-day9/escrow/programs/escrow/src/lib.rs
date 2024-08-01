596a0403db7e8f0d9bf7c0a7c84146ef97ac4e68
Olimpiu <ticudean.o@gmail.com>
2024-08-01 22:09:35 +0100

    Day 9 - Anchor and Tokens

--- cometsofweb-day9/escrow/programs/escrow/src/lib.rs
+++ cometsofweb-day9/escrow/programs/escrow/src/lib.rs
@@ -9,13 +9,24 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("Hxc7u3kNke6chmbZ9Aeg4447DzekCfRb9j8pXRKgA8yk");
+declare_id!("HmfHrs3at6GqdTvZT9D3eJL2WpsWqugF1FLEmRceir1U");
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+        instructions::take_offer::withdraw_and_close_vault(context)
     }
 }
